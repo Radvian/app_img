52bf1684e446fd959d45d132ed16f27239dde257
Radvian <58669855+Radvian@users.noreply.github.com>
2020-12-07 13:32:32 +0700

    Update streamlit-apple.py

--- streamlit-apple.py
+++ streamlit-apple.py
@@ -107,12 +107,13 @@ def main():
         st.title("Image Classifier Testing Page")
         st.markdown("Paste a link to an image and the deployed deep learning model will classify it as either a Macbook, iPad, or iPhone.")
         st.markdown("The image link should be in 'jpg', 'jpeg', or 'png' format.")
-        st.markdown("Disclaimer: There are a lot of other Apple products such as AirPods, AppleWatch, or their full tower Mac, but in this project, \
+        if st.button("See disclaimers"):
+            st.markdown("Disclaimer: There are a lot of other Apple products such as AirPods, AppleWatch, or their full tower Mac, but in this project, \
             we'll focus on the three products: Macbook, iPad, and iPhone.")
-        st.markdown("Second Disclaimer: Some image links might be forbidden to be read. If any error occurs, please try another image.")
-        st.markdown("(Initially, the author implements a file upload system, where the uploaded user file will be put into the deep learning model for classification. \
+            st.markdown("Second Disclaimer: Some image links might be forbidden to be read. If any error occurs, please try another image.")
+            st.markdown("Initially, the author implements a file upload system, where the uploaded user file will be put into the deep learning model for classification. \
             However, it only works for locally deployed web app. When the author host the web app online via share.streamlit.io, everytime we upload file, we are given with \
-            HTTP 500 error issues. This problem has been reported by the Streamlit community and is expected to be resolved in future updates to the package.)")
+            HTTP 500 error issues. This problem has been reported by the Streamlit community and is expected to be resolved in future updates to the package.")
         
         image_url = st.text_input("Paste the image file's link here")
         
