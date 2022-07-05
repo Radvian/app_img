4eaf5f41f9e3e4ab7ef9ec6b6f2d8868a60fdefc
Grady Matthias Oktavian <grady.oktavian@tokopedia.com>
2022-07-06 06:00:24 +0700

    Update streamlit-apple.py

--- streamlit-apple.py
+++ streamlit-apple.py
@@ -115,8 +115,8 @@ def main():
             However, it only works for locally deployed web app. When the author host the web app online via share.streamlit.io, everytime we upload file, we are given with \
             HTTP 500 error issues. This problem has been reported by the Streamlit community and is expected to be resolved in future updates to the package.")
          
-        st.markdown('Example of a link to an iPhone image: https://cnet1.cbsistatic.com/img/DK2ELJz5acYtFQXGiXpDcMnj0B8=/532x299/2018/06/04/6cf86c5c-687f-4755-b893-c12ecb2e1124/apple-wwdc-2018-1098.jpg')
-        st.markdown("Tips: right click on an image to 'open it on a new tab' or 'copy image address' to get the image's link")
+        st.markdown('Example of a link to an iPhone image: https://asset.kompas.com/crops/vQmkf05EkptvcC5TCHryOyL736A=/0x0:3464x2309/750x500/data/photo/2022/05/24/628c694dd631b.jpg')
+        st.markdown("Tips: right click on an image to 'copy image address' to get the image's link, then paste it to the box below.")
         
         image_url = st.text_input("Paste the image file's link here")
         
